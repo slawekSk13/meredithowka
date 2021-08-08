380586ec1e7dbf3303f07fe73795066e6c3f90ca
slawekSk13 <slawomir.skrzeczynski@gmail.com>
2021-08-08 12:13:33 +0200

    styleguide corrections

--- styleguide.config.js
+++ styleguide.config.js
@@ -1,6 +1,20 @@
 const path = require('path');
 
 module.exports = {
+    styleguideComponents: {
+        Wrapper: path.join(__dirname, 'src/styleguide/Wrapper')
+    },
+    template: {
+        favicon: 'https://meredithhouse.eu/images/xiko.png.pagespeed.ic.SdVX0AlL_p.webp',
+        head: {
+            links: [
+                {href: 'https://fonts.googleapis.com/css?family=Montserrat:100,200,300,400,500,600,900|Ubuntu:400,700&display=swap', rel: 'stylesheet'},
+                {href: 'https://fonts.googleapis.com/css2?family=Amatic+SC:wght@400;700&family=Oswald&display=swap', rel: 'stylesheet'},
+                {href:'https://fonts.gstatic.com', rel: 'preconnect'}
+            ],
+            scripts: [{src: 'https://kit.fontawesome.com/b282f36c60.js', crossorigin: 'anonymous'}]
+        }
+    },
     components: 'src/components/**/*.js',
-    ignore: ['src/components/**/*.styles.js','src/components/**/*.test.js']
+    ignore: ['src/components/**/*.styles.js', 'src/components/**/*.test.js']
 };
