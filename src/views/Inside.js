c9418b2fd3b0d2da3df76b66269f53a7c753c710
slawekSk13 <slawomir.skrzeczynski@codilime.com>
2022-04-20 14:01:58 +0200

    update

--- src/views/Inside.js
+++ src/views/Inside.js
@@ -1,29 +1,34 @@
-import {Section} from "../components/Section/Section";
-import {Title} from "../components/Title/Title";
-import {Button} from "../components/Button/Button";
-import {List} from "../components/List/List";
+import {Section} from '../components/Section/Section';
+import {Title} from '../components/Title/Title';
+import {List} from '../components/List/List';
 
 const Inside = () => {
-    return (
-        <>
-            <Section name='inside'
-                backgroundImage={`${process.env.PUBLIC_URL}/images/duze/web039.jpg`}
-            >
-                <div className='flex-title'>
-                <Title text={'Wnętrze jest zawsze większe od tego, co na zewnątrz'} light thick/>
-                <Title text={'- C.S.Lewis'} small light/>
-                </div>
-                {/*<Button icon={'fas fa-coffee'} text={'Wejdź'} target={'wyposazenie'}/>*/}
-            </Section>
-            <Section name={'wyposazenie'}>
-                <Title text={'Dom jest dwupiętrowy i składa się z:'} thick/>
-                <Title text={'Parter:'} />
-                <List listContent={['Kuchnia w pełni wyposażone w sprzęty kuchenne', 'Salon z kominkiem', 'Łazienka z prysznicem', 'Pokój z łózkiem podwójnym i pojedynczym']}/>
-                <Title text={'Pierwsze piętro:'} />
-                <List listContent={['Pokój z łózkiem podwójnym i pojedynczym', 'Pokój otwarty z łózkiem podwójnym i pojedynczym']}/>
-            </Section>
-        </>
-    )
-}
+  return (
+    <>
+      <Section name='inside' backgroundImage={`${process.env.PUBLIC_URL}/images/duze/web039.jpg`}>
+        <div className='flex-title'>
+          <Title text={'Wnętrze jest zawsze większe od tego, co na zewnątrz'} light thick />
+          <Title text={'- C.S.Lewis'} small light />
+        </div>
+      </Section>
+      <Section name={'wyposazenie'}>
+        <Title text={'Dom jest dwupiętrowy i składa się z:'} thick />
+        <Title text={'Parter:'} />
+        <List
+          listContent={[
+            'Kuchnia w pełni wyposażona w sprzęty kuchenne',
+            'Salon z kominkiem',
+            'Łazienka z prysznicem',
+            'Pokój z łóżkiem podwójnym i pojedynczym'
+          ]}
+        />
+        <Title text={'Pierwsze piętro:'} />
+        <List
+          listContent={['Pokój z łóżkiem podwójnym i pojedynczym', 'Pokój otwarty z łóżkiem podwójnym i pojedynczym']}
+        />
+      </Section>
+    </>
+  );
+};
 
-export {Inside}
+export {Inside};
--- src/views/PriceList.js
+++ src/views/PriceList.js
@@ -1,32 +1,50 @@
-import {Section} from "../components/Section/Section";
-import {Title} from "../components/Title/Title";
-import {Button} from "../components/Button/Button";
-import {List} from "../components/List/List";
-import {Paragraph} from "../components/Paragraph/Paragraph";
+import {Section} from '../components/Section/Section';
+import {Title} from '../components/Title/Title';
+import {Button} from '../components/Button/Button';
+import {List} from '../components/List/List';
+import {Paragraph} from '../components/Paragraph/Paragraph';
 
 const PriceList = () => {
-    return (
-        <>
-            <Section name='price'
-                backgroundImage={`${process.env.PUBLIC_URL}/images/web037.jpg`}
-            ><div className='flex-title'>
-                <Title text={'Czas jest cenniejszy od pieniędzy. Możesz mieć więcej pieniędzy, ale nie możesz mieć więcej czasu'} light thick/>
-                <Title text={'- Jim Rohn'} small light/>
-            </div>
-                {/*<Button icon={'fas fa-coins'} text={'Cennik'} target={'cennik'}/>*/}
-            </Section>
-            <Section name={'cennik'}>
-                <Title text={'Koszt wynajmu domu (doba):'} thick/>
-                <List listContent={['2 os. - 450 zł', '3 os. - 600 zł', '4 os. - 750 zł', '5 os. - 900 zł', '6 os. - 1000 zł', 'Od 7 osób do negocjacji', 'Dzieci do 5 lat bezpłatnie', 'Dzieci 5-10 lat - 100 zł']}/>
+  return (
+    <>
+      <Section name='price' backgroundImage={`${process.env.PUBLIC_URL}/images/web037.jpg`}>
+        <div className='flex-title'>
+          <Title
+            text={'Czas jest cenniejszy od pieniędzy. Możesz mieć więcej pieniędzy, ale nie możesz mieć więcej czasu'}
+            light
+            thick
+          />
+          <Title text={'- Jim Rohn'} small light />
+        </div>
+        {/*<Button icon={'fas fa-coins'} text={'Cennik'} target={'cennik'}/>*/}
+      </Section>
+      <Section name={'cennik'}>
+        <Title text={'Koszt wynajmu domu (doba):'} thick />
+        <List
+          listContent={[
+            '2 os. - 700 zł',
+            '3 os. - 900 zł',
+            '4 os. - 1050 zł',
+            '5 os. - 1200 zł',
+            '6 os. - 1500 zł',
+            'Od 7 osób do negocjacji',
+            'Dzieci do 3 lat bezpłatnie',
+            'Dzieci 3-10 lat - 200 zł'
+          ]}
+        />
 
-                <Paragraph text={'Doba zaczyna się o 14:00, a kończy o 11:00'} small/>
-                <Paragraph text={'Prosimy o przedpłatę w wysokości 20% wartości całego pobytu przesłać na konto:\n' +
-                'PeKaO SA 18 1240 1037 1111 0010 9422 3991'} small/>
-                <Paragraph text={'Wystawiamy faktury.'} small/>
+        <Paragraph text={'Doba zaczyna się o 14:00, a kończy o 11:00'} small />
+        <Paragraph
+          text={
+            'Prosimy o przedpłatę w wysokości 20% wartości całego pobytu przesłać na konto:\n' +
+            'PeKaO SA 18 1240 1037 1111 0010 9422 3991'
+          }
+          small
+        />
+        <Paragraph text={'Wystawiamy faktury.'} small />
+      </Section>
+    </>
+  );
+};
 
-            </Section>
-        </>
-    )
-}
-
-export {PriceList}
+export {PriceList};
